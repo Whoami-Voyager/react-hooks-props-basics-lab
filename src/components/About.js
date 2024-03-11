098884ed251ab73f1b9ca19e34877caaea7f7f11
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-03-11 14:43:41 -0600

    reworked it

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,13 @@
 import React from "react";
-import user from "../data/user"
 import UserLink from "./Links";
 
-function About({bio}) {
+function About({bio , linkedin, github}) {
   return (
     <div id="about">
       <h2>About Me</h2>
       {bio?<p>{bio}</p>:<></>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <UserLink linkedin={user.links.linkedin} github ={user.links.github}/>
+      <UserLink linkedin={linkedin} github ={github}/>
     </div>
   );
 }
